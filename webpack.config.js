e7a4c36df8653f52e0a9d858d08accc6e297aed6
jayhatha <jay@jayhathaway.com>
2018-11-14 10:43:43 -0800

    cool color thing

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const WebpackBar = require("Webpackbar");
+const WebpackBar = require("webpackbar");
 const devMode = process.env.NODE_ENV !== "production";
 const loaders = ["style-loader", "css-loader", "sass-loader"];
 module.exports = {
